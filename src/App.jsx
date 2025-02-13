5ed19a1822cb58a6143a98a2ea4dc764bd0e8581
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-13 11:40:29 +0100

    App.jsx > integrate component ProtectedRoute.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import PetsList from "./pages/PetsPage";
 import PetPage from "./pages/PetPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 // Función para comprobar si el usuario está autenticado
 const isAuthenticated = () => {
@@ -14,16 +15,12 @@ const App = () => {
   return (
     <Routes>
       {/* Si no hay un token válido, redirige de "/" a "/login" */}
-      <Route
-        path="/"
-        element={
-          isAuthenticated() ? <Navigate to="/pets" /> : <Navigate to="/login" />
-        }
+      <Route path="/" element={ isAuthenticated() ? <Navigate to="/pets" /> : <Navigate to="/login" />}
       />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/pets" element={<PetsList />} />
-      <Route path="/pet/:id" element={<PetPage />} />
+      <Route path="/pets" element={<ProtectedRoute element={<PetsList />} />} />
+      <Route path="/pet/:id" element={<ProtectedRoute element={<PetPage />} />} />
     </Routes>
   );
 };
